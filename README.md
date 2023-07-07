fe7ee8738c72e91e031b39759cbc6e02fcf4c89f
Rajdeep Singh <rajdeep.singh@orange.com>
2023-07-07 17:36:28 +0530

    ADD : Submissions for UNIX-Workbench

---
+++ README.md
@@ -0,0 +1,5 @@
+# Files Guessing Game
+
+Make was run on: Fri Jul  7 16:51:24 IST 2023
+
+Number of lines in guessinggame.sh: 22
---
+++ guessinggame.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function guess_files_count {
+  local correct_guess=0
+  local file_count=$(ls -l | grep "^-" | wc -l)
+  
+  while [[ $correct_guess -eq 0 ]]; do
+    read -p "How many files are in the current directory? " user_guess
+    
+    if [[ $user_guess -lt $file_count ]]; then
+      echo "Too low!"
+    elif [[ $user_guess -gt $file_count ]]; then
+      echo "Too high!"
+    else
+      correct_guess=1
+      echo "Congratulations! You guessed it right!"
+    fi
+  done
+}
+
+guess_files_count
+
